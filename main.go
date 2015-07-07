001aed96467700489093e996e290c06d93a91313
John Gosset <john.gosset@gmail.com>
2015-07-07 16:11:28 -0600

    Write diplomas to diplomas/course-name

--- main.go
+++ main.go
@@ -2,29 +2,89 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/gosimple/slug"
 	"github.com/jung-kurt/gofpdf"
 )
 
-func main() {
-	instructor := flag.String("i", "", "instructor's name")
-	students := flag.String("s", "", "list of students (comma-separated)")
-	dates := flag.String("d", "", "training dates")
-	base := flag.String("b", "logo.png", "base image (background)")
-	flag.Parse()
+type Diploma struct {
+	BackgroundImage string
+	Course          string
+	Dates           string
+	Instructor      string
+	Student         string
+}
 
-	txt := strings.Join([]string{*instructor, *students, *dates}, "\n")
+// DiplomaText is a string to be displayed on a Diploma, specifying coordinates
+// and font information.
+type DiplomaText struct {
+	X        float64
+	Y        float64
+	FontName string
+	FontSize int
+	Text     string
+}
 
-	// Build PDF
+// Read config from JSON file.
+func ReadConfig(fileStr string) error {
+	return nil
+}
+
+// Write config to JSON file in diplomas directory.
+func WriteConfig(fileStr string) error {
+	return nil
+}
+
+// Create a Diploma PDF and write it to disk.
+// FIXME: UTF-8 characters don't display properly, since cp1252 encoding is used
+// 		See http://godoc.org/github.com/jung-kurt/gofpdf#Fpdf.SetFont
+func CreatePDF(d *Diploma) error {
 	pdf := gofpdf.New("L", "in", "Letter", "")
 	pdf.AddPage()
-	pdf.SetFont("Times", "", 10)
-	pdf.Image(*base, 0, 0, 0, 0, false, "", 0, "")
-	pdf.Text(2, 2, txt)
-	err := pdf.OutputFileAndClose("test.pdf")
+	pdf.SetFont("Times", "", 30)
+	pdf.Image(d.BackgroundImage, 0.3, 0.3, 0, 0, false, "", 0, "")
+	pdf.Text(4, 1, d.Course)
+	pdf.Text(4, 2, d.Dates)
+	pdf.Text(4, 3, d.Instructor)
+	pdf.SetFont("Times", "", 50)
+	pdf.Text(4, 4, d.Student)
+
+	err := pdf.OutputFileAndClose(slug.Make(d.Student) + ".pdf")
 	if err != nil {
-		fmt.Println("ERROR!")
+		return err
+	}
+	return nil
+}
+
+func main() {
+	base := flag.String("b", "logo.png", "base image (background)")
+	course := flag.String("c", "Web Programming for Bureaucrats", "course name")
+	dates := flag.String("d", "July 7-10, 2015 (22.5 hours)", "training dates")
+	students := flag.String("s", "Joe Student, Jenny Student, Français Gérèçêêëà", "list of students (comma-separated)")
+	instructor := flag.String("i", "John Gosset", "instructor's name")
+	flag.Parse()
+
+	// ensure base image uses an absolute path
+	absBase, err := filepath.Abs(*base)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create PDF output dir and cd there
+	pdfDir := "diplomas/" + slug.Make(*course)
+	os.MkdirAll(pdfDir, 0755)
+	os.Chdir(pdfDir)
+
+	studentsSlice := strings.Split(*students, ",")
+	for _, s := range studentsSlice {
+		d := &Diploma{absBase, *course, *dates, *instructor, strings.TrimSpace(s)}
+		err := CreatePDF(d)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
