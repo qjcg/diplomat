8b571bd7d2636c6528b71128481f854d04f75959
John Gosset <john.gosset@gmail.com>
2015-12-04 08:23:35 -0500

    Update offsets in Default template

--- templates/default.go
+++ templates/default.go
@@ -7,10 +7,10 @@ import (
 var Default = &diploma.Template{
 	Image: "logo.png",
 	Overlay: map[string][2]float64{
-		"Student":    [2]float64{4, 1},
-		"Course":     [2]float64{4, 2},
-		"Period":     [2]float64{4, 3},
-		"Instructor": [2]float64{4, 4},
-		"Image":      [2]float64{0.3, 0.3},
+		"Student":    [2]float64{4, 200},
+		"Course":     [2]float64{4, 240},
+		"Period":     [2]float64{4, 260},
+		"Instructor": [2]float64{4, 280},
+		"Image":      [2]float64{10, 10},
 	},
 }
