a025415b2d3dfec1e21928b683f08b370bb06ced
John Gosset <john.gosset@gmail.com>
2015-12-04 08:26:20 -0500

    Update offsets in test template

--- main_test.go
+++ main_test.go
@@ -26,11 +26,11 @@ var templateTests = []Template{
 	Template{
 		Image: "logo.png",
 		Overlay: map[string][2]float64{
-			"Student":    [2]float64{4, 1},
-			"Course":     [2]float64{4, 2},
-			"Period":     [2]float64{4, 3},
-			"Instructor": [2]float64{4, 4},
-			"Image":      [2]float64{0.3, 0.3},
+			"Student":    [2]float64{300, 200},
+			"Course":     [2]float64{300, 240},
+			"Period":     [2]float64{300, 260},
+			"Instructor": [2]float64{300, 280},
+			"Image":      [2]float64{10, 10},
 		},
 	},
 }
